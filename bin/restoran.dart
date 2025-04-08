10866bfc9212b5cacd29b441d39493e9d534106a
Oleh Savenko <saveno2@medtronic.com>
2025-04-08 15:59:50 +0300

    Restoran dart

---
+++ bin/restoran.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(List<String> arguments) {
+  String day = 'Day one';
+  String reitung = ' Grand Opening';
+  int evaluation = 10;
+  stdout.write(day + reitung + ' = ' );
+  print(evaluation);
+
+  String reitung1 = ' Unsuccessful Service';
+  day='Day two';
+  evaluation =2;
+  stdout.write(day + reitung1 + ' = ');
+  print(evaluation);
+
+  String reitung3 = ' Food Critic Visit';
+  day='Day three';
+  evaluation = 3;
+  stdout.write(day + reitung3 + ' = ');
+  print(evaluation);
+
+  String reitung4 = 'Kitchen Fire ';
+  day = ' Day four';
+  evaluation = 0;
+  stdout.write(day + reitung4 + ' = ');
+  print(evaluation);
+
+  String reitung5 = 'Celebrity Dinner';
+  day = ' Day five ';
+  evaluation = 12;
+  stdout.write(day + reitung5 + ' = ');
+  print(evaluation);
+
+  String reitung6 = 'Birthday celebrations';
+  day = 'Day six ';
+  evaluation= 9;
+  stdout.write(day+ reitung6 + ' = ');
+  print(evaluation);
+
+  String reitung7 = 'Dismissal of the chef';
+  day = ' Day seven ';
+  evaluation = 2;
+  stdout.write(day+ reitung7 + ' = ' );
+  print(evaluation);
+
+
+
+}
