3133d6c43603a284e560aa6f9e3814c232baa274
Oleh Savenko <saveno2@medtronic.com>
2025-04-11 23:29:05 +0300

    Ttravel dart

---
+++ bin/trevel_time.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  int year = 2024;
+  String status = ' Just born ! ';
+  stdout.write( status);
+  print(year);
+
+  status = ' school years ';
+  year = 2030 ;
+  stdout.write(status);
+  print(year);
+
+  status = ' student years  ';
+  year = 2046;
+  stdout.write(status);
+  print(year);
+
+  status= ' work time ';
+  year = 2053;
+  stdout.write(status);
+  print(year);
+
+  status = 'TTravel  time ' ;
+  year = 2060;
+  stdout.write(status);
+  print(year);
+
+}
