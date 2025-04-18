b9c2965261c00e4e7862e045e480a9a42cb1502f
Oleh Savenko <saveno2@medtronic.com>
2025-04-18 12:06:31 +0300

    Game arufmetiks fiks

--- bin/game_arufmetiks.dart
+++ bin/game_arufmetiks.dart
@@ -1,24 +1,21 @@
 import 'dart:io';
+
 void main(List<String> arguments) {
   print('Hi lets play an arithmetic game');
   print('Player 1 enter a number');
-  String text1=stdin.readLineSync()!;
-  int numberOne = int.parse(text1);
+  int firstNumber = int.parse(stdin.readLineSync()!);
 
   print('Enter the action +, -, *, /');
   var action = stdin.readLineSync();
 
   print('Enter the following number');
-  String text2=stdin.readLineSync()!;
-   int numberTwo = int.parse(text2);
+  int secondTwo = int.parse(stdin.readLineSync()!);
 
   print('Player 2 solve the arithmetic action ');
-  stdout.write (numberOne);
-  stdout.write (action);
-  stdout.write ('= $numberTwo' );
+  stdout.write(firstNumber);
+  stdout.write(action);
+  stdout.write('= $secondTwo');
 
-
-  String text=stdin.readLineSync()!;
-  int player2 = int.parse(text);
-  print('My answer$player2');
+  int secondPlayerAnswer = int.parse(stdin.readLineSync()!);
+  print('My answer$secondPlayerAnswer');
 }
